f5aca035ec0651d7878fe956fc2af020dc3a7c71
RIRO26323 <162973457+RIRO26323@users.noreply.github.com>
2025-12-13 22:36:24 +0630

    Profile Page Done

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,11 +1,72 @@
-import {View, Text} from 'react-native'
+import {View, Text, ScrollView, Image, TouchableOpacity, Platform, ImageSourcePropType} from 'react-native'
 import React from 'react'
+import {SafeAreaView} from "react-native-safe-area-context";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import {settings} from "@/constants/data";
+import {router} from "expo-router";
+
+interface SettingsItemProps {
+    icon: ImageSourcePropType;
+    title: string;
+    onPress?: () => void;
+    textStyle?: string;
+    showArrow?: boolean
+}
+
+const SettingItems = ({icon, title, onPress, textStyle = "", showArrow = true}: SettingsItemProps) => (
+    <TouchableOpacity className="flex flex-row justify-between items-center py-3">
+        <View className="flex flex-row items-center gap-3">
+            <Image source={icon} className="size-6"/>
+            <Text className="font-rubik-medium text-lg text-black-300">{title}</Text>
+        </View>
+        {showArrow && <Image source={icons.rightArrow} className="size-5"/>}
+    </TouchableOpacity>
+)
 
 const Profile = () => {
+    const handleLogout = async () => {
+        router.replace("/sign-in")
+    }
+
     return (
-        <View>
-            <Text>Profile</Text>
-        </View>
+        <SafeAreaView className="h-full bg-white">
+            <ScrollView showsVerticalScrollIndicator={false}
+                        contentContainerClassName="pb-32 px-7">
+                <View className="flex flex-row items-center justify-between mt-5">
+                    <Text className="text-xl font-rubik-bold">Profile</Text>
+                    <Image source={icons.bell} className="size-5"/>
+                </View>
+
+                <View className="flex-row justify-center flex mt-5">
+                    <View className="flex flex-col items-center relative mt-5">
+                        <Image source={images.avatar} className="size-44"/>
+                        <TouchableOpacity className={`absolute bottom-11 ${
+                            Platform.OS === "ios" ? "right-9" : "right-2"
+                        }`}>
+                            <Image source={icons.edit} className="size-9"/>
+                        </TouchableOpacity>
+                        <Text className="font-rubik-bold text-2xl mt-2">HTET MYAT MAUNG</Text>
+                    </View>
+                </View>
+                <View className="flex flex-col mt-10">
+                    <SettingItems icon={icons.calendar} title="My Bookings"/>
+                    <SettingItems icon={icons.wallet} title="My Wallet"/>
+                </View>
+                <View className="flex flex-col mt-5 border-t pt-5 border-primary-200">
+                    {settings.slice(2).map((item, index) => (
+                        <SettingItems key={index} {...item}/>
+                    ))}
+                </View>
+
+                <View className="mt-5 border-t pt-5 border-primary-200 ">
+                    <TouchableOpacity className="flex flex-row gap-3 items-center " onPress={handleLogout}>
+                        <Image source={icons.logout} className="size-6"/>
+                        <Text className="font-rubik-medium text-lg text-red-500">Logout</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 export default Profile
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,18 +1,22 @@
-import {View, Text, ScrollView, Image, TouchableOpacity} from 'react-native'
+import {View, Text, ScrollView, Image, TouchableOpacity, Alert} from 'react-native'
 import React from 'react'
 import {SafeAreaView} from "react-native-safe-area-context";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
+import {Redirect, router} from "expo-router";
 
 const SignIn = () => {
-    const handleLogin = () =>{}
+    const handleLogin = async () => {
+        router.replace("/(root)/(tabs)")
+    }
 
     return (
         <SafeAreaView className={"h-full bg-white"}>
             <ScrollView contentContainerClassName={"h-full"}>
                 <Image source={images.onboarding} resizeMode="contain" className={"w-full h-4/6"}/>
                 <View className={"px-10"}>
-                    <Text className={"text-base text-center uppercase font-rubik text-black-200"}>Welcome to Estate</Text>
+                    <Text className={"text-base text-center uppercase font-rubik text-black-200"}>Welcome to
+                        Estate</Text>
                     <Text className={"text-3xl text-center mt-2 font-rubik-bold text-black-300"}>
                         Let Get You Closer To {"\n"}
                         <Text className="text-primary-300">
@@ -22,7 +26,8 @@ const SignIn = () => {
                     <Text className={"text-lg font-rubik text-black-200 text-center mt-12"}>
                         Login Estate With Google
                     </Text>
-                    <TouchableOpacity onPress={handleLogin} className={"bg-white rounded-full shadow-md shadow-zinc-300 w-full py-4 mt-5 elevation-2xl"}>
+                    <TouchableOpacity onPress={handleLogin}
+                                      className={"bg-white rounded-full shadow-md shadow-zinc-300 w-full py-4 mt-5 elevation-2xl"}>
                         <View className={"flex flex-row items-center justify-center"}>
                             <Image source={icons.google} className={"w-5 h-5"} resizeMode="contain"/>
                             <Text className={"text-lg font-rubik-medium text-black-20 ml-2"}>Continue With Google</Text>
